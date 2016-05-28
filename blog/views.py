746110fe440d203d4e39761a7cb0b249b2b9dd72
Jbradley123 <jbradley123@gmail.com>
2016-05-28 15:52:16 -0400

    Extensions tutorial. Added publish and delete functionality.

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #removed this line for drafts
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -43,3 +43,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+# Publish button
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+# Remove posts
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+    
